a4b909369dacf03ec814bf93b6b1ff1367140cbe
kamabata <horizon0711pc@gmail.com>
2024-07-24 09:43:08 +0900

    keyのサイズ確認

--- cpp/graph_id.cpp
+++ cpp/graph_id.cpp
@@ -56,6 +56,7 @@ std::string GraphIDGenerator::get_long_distance_id(const Structure &structure) c
                 std::cout << "for (const auto& [key, value] : sg.graph_map){" << std::endl;
                 if (std::get<0>(key) == j){
                     std::cout << "if (std::get<0>(key) == j){" << std::endl;
+                    std::cout << "key size: " << std::tuple_size<decltype(key)>::value << std::endl;
                     sg.break_edge(get<0>(key), get<1>(key), get<2>(key), true);
                     std::cout << "sg.break_edge(get<0>(key), get<1>(key), get<2>(key), true);" << std::endl;
                 }
