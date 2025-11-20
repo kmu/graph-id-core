19f6e6f766d8b00fd35d80b90de191107b167dec
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2025-11-20 12:42:37 +0900

    refactor: Refactor date handling and file writing logic
    - Refactor file writing logic to use `Path` for file opening
    - Improve code readability by replacing date formatting logic with `now()` and `strftime()` methods
    - Enhance code maintainability by changing import statement from `datetime` to `Path`

--- graph_id/commands/gid.py
+++ graph_id/commands/gid.py
@@ -1,6 +1,6 @@
 import argparse
 import csv
-from datetime import datetime
+from pathlib import Path
 
 from pymatgen.core import Structure
 from tabulate import tabulate
@@ -37,9 +37,6 @@ if __name__ == "__main__":
 
     print(tabulate(table, headers=t_headers))
 
-    now = datetime.now()
-    now_str = now.strftime("%Y%m%d-%H%M%S")
-
-    with open(f"graph_id_{now_str}.csv", "w") as f:
+    with Path("graph_id.csv").open("w") as f:
         writer = csv.writer(f)
         writer.writerows(table)
