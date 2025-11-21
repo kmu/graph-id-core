f38c95bcae96bb696ed6571cc856d2cda2de286a
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2025-11-21 13:17:28 +0900

    fix test path

--- tests/cpp/test_graph_id.py
+++ tests/cpp/test_graph_id.py
@@ -10,7 +10,7 @@ import graph_id
 
 from .imports import graph_id_cpp
 
-test_file_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../graph_id/tests/test_files"))
+test_file_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "../py/test_files"))
 
 
 def small_test_structure(max_sites=30):
